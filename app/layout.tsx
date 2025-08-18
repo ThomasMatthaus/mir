7e8b83071f09b54886711e7636cc88666864aa02
ThomasMatthaus <mirqoslav@yandex.ru>
2025-08-18 17:01:08 +0000

    Fix Google Fonts AbortError

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ['latin', 'cyrillic'] });
+const inter = Inter({ subsets: ['latin', 'cyrillic'], display: 'optional' });
 
 export const metadata: Metadata = {
   title: 'Мир Недвижимости - Агентство недвижимости',
